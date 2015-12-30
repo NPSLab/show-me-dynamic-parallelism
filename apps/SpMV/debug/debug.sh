ba01769dbc6f35ca3d557ac2e66abcdfd8a8508e
Da Li <daniel.lee.bit@gmail.com>
2015-12-29 23:44:16 -0500

    + Add debug.sh for SpMV

---
+++ apps/SpMV/debug/debug.sh
@@ -0,0 +1,11 @@
+./gpu-SpMV -f 0 -i data_test.gr -v -d 
+#./gpu-ir-synth -f 0 -i data_test.gr -v -d
+#./gpu-ir-synth -f 2 -i ../../../data/p2p-Gnutella05.txt -v -d -s 1
+#./gpu-ir-synth -f 0 -i ../../../data/USA-road-d.COL.gr -v -d -s 2
+#./gpu-ir-synth -f 0 -i ../../../data/USA-road-d.COL.gr -v -d -s 3
+#./gpu-ir-synth -f 0 -i ../../../data/USA-road-d.COL.gr -v -d -s 4
+#./gpu-ir-synth -f 0 -i ../../../data/USA-road-d.COL.gr -v -d -s 5
+#./gpu-SpMV -f 1 -i ../../../data/asia.osm.graph -v -s 0
+#./gpu-SpMV -f 2 -i ../../../data/p2p-Gnutella05.txt -v -s 0
+#./gpu-SpMV -f 0 -i data_test.gr -v -d -s 0
+
