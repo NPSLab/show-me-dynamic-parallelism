0574968393d56147ac6c4de6f16de8fa9464d3ff
Da Li <daniel.lee.bit@gmail.com>
2016-04-10 11:15:45 -0400

    + Add kron_log16 for BFS-Rec

--- apps/bfs-rec/run/run.sh
+++ apps/bfs-rec/run/run.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-BIN=../src/gpu/gpu-bfs-rec
+BIN=../src/gpu-bfs-rec
 #DATA_FILE=../../../datasets/DIMACS9/USA-road-d.COL.gr
-#DATA_FILE=../../../datasets/DIMACS10/coPapersCiteseer.graph
-DATA_FILE=../../../datasets/SLNDC/wiki-Vote.txt
+#DATA_FILE=../../../datasets/DIMACS10/kron_g500-simple-logn18.graph
+DATA_FILE=../../../datasets/DIMACS10/kron_g500-simple-logn16.graph
+#DATA_FILE=../../../datasets/SLNDC/wiki-Vote.txt
 #DATA_FILE=../../../datasets/SLNDC/amazon0505.txt
 LOG_FILE=bfs_rec_performance.log
 
@@ -11,8 +12,7 @@ for i in 64
 do
 #	EXE=$BIN-t$i
 	EXE=$BIN
-	$EXE -f 2 -i ${DATA_FILE} -v -s 0 #>> ${LOG_FILE}
-	$EXE -f 2 -i ${DATA_FILE} -v -s 1 #>> ${LOG_FILE}
-	$EXE -f 2 -i ${DATA_FILE} -v -s 2 #>> ${LOG_FILE}
-	$EXE -f 2 -i ${DATA_FILE} -v -s 3 #>> ${LOG_FILE}
+	$EXE -f 1 -i ${DATA_FILE} -v -s 0 #>> ${LOG_FILE}
+	$EXE -f 1 -i ${DATA_FILE} -v -s 4 #>> ${LOG_FILE}
+	$EXE -f 1 -i ${DATA_FILE} -v -s 5 #>> ${LOG_FILE}
 done
